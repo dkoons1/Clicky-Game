bd39ea92bf39560b738396634776dc6df634f0e8
Daniel Joji Koons <jojikoons@gmail.com>
2020-03-10 18:39:08 -0700

    Updates

---
+++ precache-manifest.324eeea4f1c6ef55f7be1621a628341b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57971cd264b32a5bbdc794e14dbd7245",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "aa079a05ebf9ef65bfb9",
+    "url": "/Clicky-Game/static/css/main.1429e0e4.chunk.css"
+  },
+  {
+    "revision": "22d299f6498e48e1ff50",
+    "url": "/Clicky-Game/static/js/2.92680def.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Clicky-Game/static/js/2.92680def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa079a05ebf9ef65bfb9",
+    "url": "/Clicky-Game/static/js/main.5496e342.chunk.js"
+  },
+  {
+    "revision": "e2fd7d53299e4e5eadb0",
+    "url": "/Clicky-Game/static/js/runtime-main.9dcfad0a.js"
+  }
+]);
